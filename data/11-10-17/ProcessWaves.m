2730efec28c60872f5a6dc404c4b1b2622df1f25
Sunny3oy <meisunny1992@hotmail.com>
2017-11-20 00:54:15 -0500

    pushing for ichwan

--- data/11-10-17/ProcessWaves.m
+++ data/11-10-17/ProcessWaves.m
@@ -1,4 +1,5 @@
 function ProcessWaves(file)
+close all
 Fs = 44100;
 load(file) %remove this line when copying the entire script to test for sound
 data = trim_cardboard_vol15;% the data name
@@ -8,10 +9,11 @@ figure(1)
 num_bins = length(a);
 plot([0:1/(num_bins/2 -1):1], a(1:num_bins/2))
 %plot(a(1:1000))
-xlabel('Normalised frequency (\pi rads/sample)')
+title('FFT of Raw Data')
+xlabel('Normalised frequency')
 ylabel('Magnitude')
 %create Butter filter co
-[B, A] = butter(2, 0.3, 'low');
+[B, A] = butter(20, 0.3, 'high');
 %apply Butter filter
 filtered = filter(B,A,data);
 
@@ -20,13 +22,24 @@ b = abs(fft(filtered));
 figure(2)
 num_bins = length(b);
 plot([0:1/(num_bins/2 -1):1], b(1:num_bins/2))
-xlabel('Normalised frequency (\pi rads/sample)')
+title('FFT of Butter filtered data')
+xlabel('Normalised frequency')
 ylabel('Magnitude')
 %testing the sound difference
 %1st sound is original
 %2nd sound is filtered
 %sound(data,Fs);
 %sound(filtered,Fs);
+figure(3)
+plot(trim_cardboard_vol15)
+xlabel('Time')
+ylabel('Amplitude')
+title('Raw data from LDV')
+figure(4)
+plot(filtered)
+xlabel('Time')
+ylabel('Amplitude')
+title('Filtered Data with Butter')
 
 
 
